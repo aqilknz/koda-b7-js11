c607549cdf1489f7801546c55b0d849b9afdde89
aqilknz <154092951+aqilknz@users.noreply.github.com>
2026-02-27 15:42:58 +0700

    Create minitask-11.js

---
+++ minitask-11.js
@@ -0,0 +1,45 @@
+
+// function showName(name, time) {
+    
+//     setTimeout (function () {
+    
+//     })
+// }
+
+// const showName = new Promise ((resolve,reject) => {
+//     let name= ""
+//     let time = 0
+//     if 
+// })
+
+
+// const promise = (username, time) => {
+//     new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             username
+//     }, time);
+//   });
+// };
+
+// const John = () => {
+//     new Promise((resolve, _) => {
+//         setTimeout(() => resolve('John'), 1500);
+//     })
+//     .then(result => console.log(result))
+//     .catch(error => console.error(error));
+// }
+showName
+    .then(value => {
+        console.log
+    })
+
+const runProgram = async () => {
+  try {
+    await showName("John", 1500);
+    await showName("Ed", 2000);
+    await showName("Jane", 500);
+    
+  } catch (err) {
+    console.log("Error:", err);
+  }
+};
